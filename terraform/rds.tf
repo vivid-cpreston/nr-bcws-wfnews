c937c33d9003388ca09796ee09abb0e929081920
VIVIDSOLUTIONS\cpreston <cpreston@vividsolutions.com>
2022-08-25 17:10:02 -0700

    added resources to allow external DB access

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -18,3 +18,31 @@ resource "aws_db_instance" "wfnews_pgsqlDB"{
     tags = local.common_tags
     db_subnet_group_name = aws_db_subnet_group.wfnews_db_subnet_group.name
 }
+
+resource "aws_db_proxy" "wfnews_db_proxy" {
+  name                   = "wfnews-db-proxy-${var.target_env}"
+  debug_logging          = false
+  engine_family          = "POSTGRESQL"
+  idle_client_timeout    = 1800
+  require_tls            = true
+  role_arn               = data.aws_iam_role.wfnews_automation_role.arn
+  vpc_security_group_ids = [data.aws_security_group.web.id, aws_security_group.wfnews_ecs_tasks.id]
+  vpc_subnet_ids         = [module.network.aws_subnet_ids.app.ids]
+
+  auth {
+    auth_scheme = "SECRETS"
+    iam_auth    = "DISABLED"
+  }
+
+  tags = local.common_tags
+}
+
+resource "aws_db_proxy_default_target_group" "wfnews_db_target_group" {
+  db_proxy_name = aws_db_proxy.wfnews_pgsqlDB_proxy.name
+}
+
+resource "aws_db_proxy_target" "wfnews_db_proxy_target" {
+  db_instance_identifier = aws_db_instance.wfnews_pgsqlDB.id
+  db_proxy_name          = aws_db_proxy.wfnews_db_proxy.name
+  target_group_name      = aws_db_proxy_default_target_group.wfnews_db_target_group.name
+}
